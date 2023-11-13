32c44391e5b86761db702a2df4d1a57b90aad5e8
Zach <zag0003@auburn.edu>
2023-11-12 22:25:09 -0600

    clear cookies works but redirects to unauthenticated page

--- common/js/clear-button.js
+++ common/js/clear-button.js
@@ -1,10 +1,14 @@
 
-function clear() {
-    localStorage.clear();
-    console.log('cookies cleared!');
-    window.location.href = window.location.href + '/clear-success'
-}
-
-window.onload(() => {
-    document.querySelector('#cookie-clear').onclick = clear;
-})
+(() => {
+    function clear() {
+        localStorage.clear();
+        console.log('cookies cleared!');
+        window.location.href = window.location.href + '/clear-success'
+    }
+    
+    window.onload = () => {
+        console.log('loadead!')
+        const clearButton = document.getElementById('cookie-clear')
+        clearButton.addEventListener('click', clear)
+    }
+})()
